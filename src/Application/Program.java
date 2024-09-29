105d9b421a00f51eecc4fedd625555a08240f19d
CesarCabral1986 <cesarcabral@outlook.com>
2024-09-29 19:38:22 -0300

    Seller entity class

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,8 +1,10 @@
 package Application;
 
+import java.sql.Date;
 import java.util.Scanner;
 
 import Model.Entities.Department;
+import Model.Entities.Seller;
 
 public class Program {
 
@@ -11,7 +13,8 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(01, 01, 2020), 3000.0, obj);
+		System.out.println(seller);
 		
 		
 		sc.close();
