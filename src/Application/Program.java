6572f4a11b0de902f09b366e75928ffcc122a81d
CesarCabral1986 <cesarcabral@outlook.com>
2024-09-30 21:16:41 -0300

    Find by id implementation

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 
 import Model.Entities.Department;
 import Model.Entities.Seller;
+import Model.dao.DaoFactory;
+import Model.dao.SellerDao;
 
 public class Program {
 
@@ -13,8 +15,14 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		
 		Department obj = new Department(1, "Books");
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(1950), 3000.0, obj);
-		System.out.println(seller);
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(0), 3000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+
+		Seller seller1 = sellerDao.findById(3);
+		
+		System.out.println(seller1);
 		
 		
 		sc.close();
